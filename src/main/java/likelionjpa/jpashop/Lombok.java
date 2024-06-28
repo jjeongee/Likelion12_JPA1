fd0bd43a1df1a226fa356e757111b5fc0595dd94
jjeongee <starstar117@naver.com>
2024-06-28 23:01:56 +0900

    h2-db connected

---
+++ src/main/java/likelionjpa/jpashop/Lombok.java
@@ -0,0 +1,9 @@
+package likelionjpa.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Lombok {
+    public String data;
+}
---
+++ src/main/java/likelionjpa/jpashop/Member.java
@@ -0,0 +1,15 @@
+package likelionjpa.jpashop;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id; //pk
+    private String username;
+
+}
